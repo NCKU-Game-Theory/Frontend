b2085827a5c3d3c1bc761add67eacd7d15007bb9
ysh <mysh212@gmail.com>
2026-04-10 02:26:38 +0800

    Init commit

---
+++ src/plugins/framework.vue
@@ -0,0 +1,75 @@
+<template>
+    <v-snackbar-queue v-model = 'toast' total-visible = 5 />
+    <router-view />
+</template>
+
+<script lang="ts" setup>
+    import { onMounted, provide, ref, type Ref } from 'vue';
+
+    // Properties for other general modules
+    const inited = ref(false);
+    const loading = ref(false);
+    provide('loading', loading);
+    provide('inited', inited);
+    
+    // General function
+    const toast: Ref<{[id: string]: any}[]> = ref([]);
+
+    const error = (error: string) => {
+        toast.value.push({
+            text: error,
+            prependIcon: 'mdi-alert',
+            color: 'pink',
+            timer: 'bottom'
+        })
+    }
+
+    const info = (x: string) => {
+        toast.value.push({
+            text: x,
+            prependIcon: 'mdi-check-circle',
+            color: 'teal',
+            timer: 'bottom'
+        })
+    }
+    
+    provide('error', error);
+    provide('info', info);
+    provide('toast', toast);
+
+    // toast API
+    const qerror = (x: string) => {
+        localStorage.setItem('message_queue', x);
+        localStorage.setItem('message_type', 'error');
+    }
+    const qinfo = (x: string) => {
+        localStorage.setItem('message_queue', x);
+        localStorage.setItem('message_type', 'info');
+    }
+    onMounted(() => {
+        const message = localStorage.getItem('message_queue');
+        if(message == null) return;
+        const type = localStorage.getItem('message_type');
+
+        localStorage.removeItem('message_queue');
+        localStorage.removeItem('message_type');
+
+        switch(type) {
+            case 'error':
+                error(message);
+                break;
+            case 'info':
+                info(message);
+                break;
+            default:
+                info(message);
+        }
+    })
+    const kick = (x: string) => {
+        qerror(x);
+        location.href = '/';
+    }
+    provide('qerror', qerror);
+    provide('kick', kick);
+    provide('qinfo', qinfo);
+</script>
---
+++ src/plugins/layout.vue
@@ -0,0 +1,70 @@
+<template>
+    <v-app>
+        <v-app-bar
+        color = grey
+        class = elevate
+        scroll-behavior = 'collapse'
+        elevate = 3
+        
+        rounded
+        >
+        <template #prepend>
+            <v-app-bar-nav-icon>
+                <a class = 'nodeco' href = '/'>
+                    <v-progress-circular indeterminate v-if = loading />
+                    <v-icon class = ma-1 v-else> mdi-toolbox </v-icon>
+                </a>
+            </v-app-bar-nav-icon>
+            <v-app-bar-title>
+                    <a class = 'nodeco' href = '/'>
+                        <slot name = 'title'>
+                            {{ title }}
+                        </slot>
+                    </a>
+                </v-app-bar-title>
+            </template>
+        </v-app-bar>
+        <v-main>
+            <v-progress-linear :indeterminate = loading bg-color = transparent />
+            <slot />
+        </v-main>
+    </v-app>
+</template>
+
+<script lang = ts setup>
+    import { inject, type Ref } from 'vue';
+    
+    const error: Function = inject('error')!;
+    const loading: Ref<boolean> = inject('loading')!;
+
+    const props = defineProps({
+        title: {
+            type: String,
+            default: "Sphinx"
+        }
+    })
+</script>
+
+<style>
+body {
+  background-color: white;
+}
+.v-card-title {
+    font-weight: 900 !important;
+}
+.nodeco {
+    text-decoration: none !important;
+    color: inherit;
+}
+.v-app-bar {
+    position: fixed !important;
+}
+.v-toolbar {
+    backdrop-filter: blur(10px);
+    background-color: rgba(150, 150, 150, .75);
+}
+.v-toolbar:hover {
+    backdrop-filter: none;
+    background-color: rgb(150, 150, 150);
+}
+</style>
